05705c3f3930cb464b778d991dfd475c198fef46
Simon <tengentoppa@hotmail.com.tw>
2021-05-17 10:59:29 +0800

    + Vue practice

--- Vue/Hello world/index.js
+++ Vue/Hello world/index.js
@@ -1,7 +1,66 @@
-var app = new Vue({
-    el: '#app',
+var app1 = new Vue({
+    el: '#app-1',
     data: {
         message: 'Hello Vue'
     }
 })
-app.message = 'test';
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn Javascript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesom' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
---
+++ Vue/todo_list/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <ToDoList />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import ToDoList from "./components/TodoList.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+    ToDoList,
+  },
+});
+</script>
